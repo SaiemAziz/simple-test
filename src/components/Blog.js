bf0bff9243dd16fc2a896e9080b0f0b6b944407e
SaiemAziz <sayemazizchy@gmail.com>
2022-10-11 04:07:43 +0600

    question answer added

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import { Accordion } from "flowbite-react";
+import React from "react";
 
 const Blog = () => {
-    return (
-        <div>
-            <h1>Blog</h1>
-        </div>
-    );
+  return (
+    <Accordion style={{"margin-top": "50px"}} flush={true}>
+      <Accordion.Panel>
+        <Accordion.Title>Purpose of react router?</Accordion.Title>
+        <Accordion.Content>
+          <p className="mb-2 text-gray-500">
+          React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.
+          </p>
+        </Accordion.Content>
+      </Accordion.Panel>
+      <Accordion.Panel>
+        <Accordion.Title>How context API works?</Accordion.Title>
+        <Accordion.Content>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+          The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on.
+          </p>
+        </Accordion.Content>
+      </Accordion.Panel>
+      <Accordion.Panel>
+        <Accordion.Title>
+          Describe something about useRef hook?
+        </Accordion.Title>
+        <Accordion.Content>
+          <p className="mb-2 text-gray-500 dark:text-gray-400">
+          The useRef is a hook that allows to directly create a reference to the DOM element in the functional component. Syntax: const refContainer = useRef(initialValue); The useRef returns a mutable ref object.
+          </p>
+          
+        </Accordion.Content>
+      </Accordion.Panel>
+    </Accordion>
+  );
 };
 
 export default Blog;
