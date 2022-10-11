2fabcd6e8e9598a07b1f6e09bc2343c373680620
SaiemAziz <sayemazizchy@gmail.com>
2022-10-11 21:30:47 +0600

    topic card updated

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -5,7 +5,8 @@ const Topic = ({topic}) => {
     return (
         <div className='bg-yellow-800 rounded-xl text-white overflow-hidden shadow-2xl'>
             <img className='p-5 pb-2 mx-auto w-full' src={topic.logo} alt="" />
-            <h1 className='text-xl mb-2 italic font-medium'>{topic.name}</h1>
+            <h1 className='text-xl italic font-medium mb-2 '><span className='block text-sm font-bold not-italic text-amber-600'>{topic.total} questions</span> {topic.name} </h1>
+            
             <Link to={`topic/${topic.id}`}>
                 <button 
                 className='w-full hover:bg-yellow-400 sm:hover:scale-125 hover:text-yellow-900 py-3 font-bold duration-200 bg-yellow-600 text-yellow-300'
